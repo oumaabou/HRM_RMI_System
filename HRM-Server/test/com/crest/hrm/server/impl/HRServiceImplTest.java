0b403f08b53b5091179bb17950af75c70ee90167
Malika <xinmalie@gmail.com>
2026-04-08 23:35:03 +0800

    Added 3 test files will put in code later

---
+++ HRM-Server/test/com/crest/hrm/server/impl/HRServiceImplTest.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.crest.hrm.server.impl;
+
+/**
+ *
+ * @author Malika
+ */
+public class HRServiceImplTest {
+    
+}
